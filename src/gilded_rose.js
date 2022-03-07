2532cdec567dd2bf25ac34d6e381ee6b004dfb9d
jpasikainen <asikainen.juhapekka@gmail.com>
2022-03-07 14:21:43 +0200

    refactor

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -44,10 +44,8 @@ class Shop {
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != "Aged Brie") {
           if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
+            if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+              this.decreaseQuality(i);
             }
           } else {
             this.items[i].quality = 0;
