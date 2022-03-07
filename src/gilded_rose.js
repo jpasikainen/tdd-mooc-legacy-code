4838af9331814154ad2843685c6c5ad6fb8af32d
jpasikainen <asikainen.juhapekka@gmail.com>
2022-03-07 14:16:10 +0200

    refactor

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -29,9 +29,7 @@ class Shop {
           this.items[i].quality = this.items[i].quality + 1;
           if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
             if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.increaseQuality(i);
-              }
+              this.increaseQuality(i);
             }
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
